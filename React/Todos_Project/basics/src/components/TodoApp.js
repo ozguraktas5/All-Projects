138652d982a7303cf7fc09ad274f5d880bfbf10f
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-06-17 20:26:21 +0300

    v1

---
+++ React/Todos_Project/basics/src/components/TodoApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import "./style.scss";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+const TodoApp = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodos("");
+    }
+  };
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <Container className="todo-list">
+      <h1>Todo List</h1>
+
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+
+      <TodoList list={todos} remove={deleteTodo} />
+    </Container>
+  );
+};
+
+export default TodoApp;
