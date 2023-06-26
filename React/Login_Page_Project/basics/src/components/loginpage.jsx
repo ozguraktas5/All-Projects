6896215aa0949aef8068319185230b9eb688ccdc
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-06-26 13:05:22 +0300

    v1

--- React/Login_Page_Project/basics/src/components/loginpage.jsx
+++ React/Login_Page_Project/basics/src/components/loginpage.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import backgroundImage from '../assets/image/photo-1508615039623-a25605d2b022.avif';
-
+import React from "react";
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  Form,
+  ButtonGroup,
+} from "react-bootstrap";
+import "../components/css/style.css";
+import image from "../assets/image/banner.png";
+import google from "../assets/icons/google.svg";
+import github from "../assets/icons/github.svg";
+import facebook from "../assets/icons/facebook.svg";
 
 const LoginPage = () => {
   return (
-    <div>
-      React
-    </div>
-  )
-}
+    <Container className="container">
+      <Row className="page-all">
+        <Col className="loginpage" lg={4}>
+          <h4>Your logo</h4>
+          <h3>Login</h3>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="lp-label">Email address</Form.Label>
+              <Form.Control type="email" placeholder="username@gmail.com" />
+            </Form.Group>
 
-export default LoginPage
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className="lp-label">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+              <a className="forgot-pass" href="#">
+                Forgot Password?
+              </a>
+            </Form.Group>
+
+            <Button className="w-100" variant="primary" type="submit">
+              Sign in
+            </Button>
+
+            <p>or continue with</p>
+            <ButtonGroup>
+              
+              <Button>
+                <img src={google} alt="google" />
+              </Button>
+              <Button>
+                <img src={github} alt="github" />
+              </Button>
+              <Button>
+                <img src={facebook} alt="facebook" />
+              </Button>
+            </ButtonGroup>
+            <p>Don't have an account yet?<a href="#" className="lp-register"> Register for free</a></p>
+          </Form>
+        </Col>
+        <Col className="image" lg={6}>
+          <img src={image} alt="banner" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LoginPage;
