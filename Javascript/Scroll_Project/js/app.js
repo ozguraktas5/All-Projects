995dabc066d2a8378f11ecec958a423dd24cd0c6
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-06-28 17:35:13 +0300

    v1

--- Javascript/Scroll_Project/js/app.js
+++ Javascript/Scroll_Project/js/app.js
@@ -1,13 +1,27 @@
 const navEl = document.querySelector(".navbar");
-var scrollTimer;
+let aElements = document.querySelectorAll("li a");
+let topLinkEl = document.querySelector(".footer .top-link");
+let scrollTimer;
+
+
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
     navEl.classList.add("fixed-nav");
     navEl.classList.remove("navbar");
+    topLinkEl.classList.add("show-link");
+    for (let i = 0; i < aElements.length; i++) {
+      aElements[i].classList.add("dark-text");
+}
+    
   } else {
     navEl.classList.add("navbar");
     navEl.classList.remove("fixed-nav");
+    topLinkEl.classList.remove("show-link");
+    for (let i = 0; i < aElements.length; i++) {
+      aElements[i].classList.remove("dark-text");
+}
+    
     clearTimeout(scrollTimer);
     scrollTimer = setTimeout(function () {
       navEl.classList.remove("fixed-nav");
--- React/Login_Page_Project/basics/src/components/loginpage.jsx
+++ React/Login_Page_Project/basics/src/components/loginpage.jsx
@@ -8,50 +8,97 @@ import {
   ButtonGroup,
 } from "react-bootstrap";
 import "../components/css/style.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
 import image from "../assets/image/banner.png";
 import google from "../assets/icons/google.svg";
 import github from "../assets/icons/github.svg";
 import facebook from "../assets/icons/facebook.svg";
 
 const LoginPage = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email("Gecerli bir email adresi giriniz!")
+      .min(5, "Email en az 5 karakterden olusmalidir!")
+      .max(50, "Email en fazla 50 karakterden olusmalidir!")
+      .required("Email kismini bos birakamazsiniz!"),
+    password: yup
+      .string()
+      .min(8, "Parola en az 8 karakterden olusmalidir!")
+      .max(50, "Parola en fazla 50 karakterden olusmalidir!")
+      .required("Parola kismini bos birakamazsiniz!")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+        "Parola en az bir büyük harf, bir kücük harf, bir sayi ve bir özel karakter icermelidir!"
+      ),
+  });
+
+  const onSubmit = async (values) => {
+    const dto = {
+      email: values.email,
+      password: values.password
+    }
+
+    
+  }
   return (
     <Container className="container">
       <Row className="page-all">
         <Col className="loginpage" lg={4}>
-          <h4>Your logo</h4>
-          <h3>Login</h3>
+          <h5 className="your-logo">Your logo</h5>
+          <h4 className="your-login">Login</h4>
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className="lp-label">Email address</Form.Label>
-              <Form.Control type="email" placeholder="username@gmail.com" />
+              <Form.Control
+                className="lp-input"
+                type="email"
+                placeholder="username@gmail.com"
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className="lp-label">Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                className="lp-input"
+                type="password"
+                placeholder="Password"
+              />
               <a className="forgot-pass" href="#">
                 Forgot Password?
               </a>
             </Form.Group>
 
-            <Button className="w-100" variant="primary" type="submit">
-              Sign in
+            <Button className="w-100" variant="success" type="submit">
+              Login
             </Button>
 
-            <p>or continue with</p>
-            <ButtonGroup>
-              
-              <Button>
+            <p className="lp-continue">or continue with</p>
+            <ButtonGroup className="buttons">
+              <Button className="lp-google">
                 <img src={google} alt="google" />
               </Button>
-              <Button>
+              <Button className="lp-github">
                 <img src={github} alt="github" />
               </Button>
-              <Button>
+              <Button className="lp-facebook">
                 <img src={facebook} alt="facebook" />
               </Button>
             </ButtonGroup>
-            <p>Don't have an account yet?<a href="#" className="lp-register"> Register for free</a></p>
+            <p className="lp-account">
+              Don't have an account yet?
+              <a href="#" className="lp-register">
+                {" "}
+                Register for free
+              </a>
+            </p>
           </Form>
         </Col>
         <Col className="image" lg={6}>
