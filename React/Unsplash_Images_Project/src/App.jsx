568645aeee888d02d9e0432b85cc5874c360a93b
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-07-13 14:51:48 +0300

    v1

--- React/Unsplash_Images_Project/src/App.jsx
+++ React/Unsplash_Images_Project/src/App.jsx
@@ -1,29 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button, Container } from "react-bootstrap";
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 function App() {
-  
+  const [searchText, setSearchText] = useState("");
+  const [result, setResult] = useState([]);
+  const [sun, setSun] = useState(true);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = sun ? "white" : "black";
+  }, [sun]);
+
+  const handleClick = () => {
+    setSun(!sun);
+  };
+
+  const handleSearch = () => {
+    const query = searchText;
+
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${query}&client_id=I8X2yX1ZJxmbA0Q15SRLNFx2oyIIveKox-ob74REVnI`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.results || [];
+        console.log(data);
+        setResult(results);
+      })
+
+      .catch((error) => console.log(error));
+  };
 
   return (
-    
-      <Container>
-        <Button className="icon-button">
-          <LightModeIcon className="light-mode"/>
-          <DarkModeIcon className="dark-mode"/>
+    <Container>
+      <Button className="icon-button">
+        {sun ? (
+          <LightModeIcon className="light-mode" onClick={handleClick} />
+        ) : (
+          <DarkModeIcon className="dark-mode" onClick={handleClick} />
+        )}
+      </Button>
+      <h1>Unsplash Images</h1>
+      <div className="input-button">
+        <input
+          className="input-light"
+          type="search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Button
+          className="success-button"
+          variant="success"
+          onClick={handleSearch}
+        >
+          Search
         </Button>
-        <h1>Unsplash Images</h1>
-        <div className="input-button">
-          <input type="search" placeholder="job"/>
-          <Button className="success-button" variant="success">Search</Button>
-        </div>
-        <div className="all-image">
-
-        </div>
-      </Container>
-    
+      </div>
+      <div className="all-image">
+        {result &&
+          result.map((item, index) => (
+            <img key={index} src={item.urls.small} alt="images" />
+          ))}
+      </div>
+    </Container>
   );
 }
 
